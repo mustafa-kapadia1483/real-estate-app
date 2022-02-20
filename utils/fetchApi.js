f8a3e096640fb6096ab8ab045a8189c27d1e6a84
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-02-20 12:58:41 +0530

    util function for fetching data

---
+++ utils/fetchApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const baseUrl = "bayut.p.rapidapi.com";
+
+export const fetchApi = async url => {
+  const { data } = await axios.get(url, {
+    headers: {
+      "x-rapidapi-host": "bayut.p.rapidapi.com",
+      "x-rapidapi-key": "bc961b5732msh6369302c166bb81p17e8fajsncbfc6e2abd26",
+    },
+  });
+  return data;
+};
