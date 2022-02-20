3909e9640adbff7231677fb2c823a2fe833ebdde
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-02-20 13:04:42 +0530

    url fix

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = "bayut.p.rapidapi.com";
+export const baseUrl = "https://bayut.p.rapidapi.com";
 
 export const fetchApi = async url => {
   const { data } = await axios.get(url, {
